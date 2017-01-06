6062c3cc683fab681a2b09528305e6d63f5bc2d4
Stephen Newkirk <newkirk771@gmail.com>
2017-01-06 13:48:33 -0800

    Complete instance model.

--- app/models/instance.js
+++ app/models/instance.js
@@ -1,5 +1,10 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+  timestamp: DS.attr(),
+  hasDuration: DS.attr(),
+  hasCount: DS.attr(),
+  duration: DS.attr(),
+  count: DS.attr(),
+  habit: belongsTo('habit', {async: true})
 });
