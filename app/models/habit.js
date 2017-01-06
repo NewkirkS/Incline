907996f939126c019507b09f92a3122e926ecf21
Stephen Newkirk <newkirk771@gmail.com>
2017-01-06 13:40:12 -0800

    Complete habit model.

--- app/models/habit.js
+++ app/models/habit.js
@@ -1,5 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+  name: DS.attr(),
+  user: belongsTo('user', {async: true}),
+  instances: DS.hasMany('instance', {async: true})
 });
