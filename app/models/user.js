0f66b7db99777577af83d03479cc2aa36238213d
Stephen Newkirk <newkirk771@gmail.com>
2017-01-06 12:35:34 -0800

    Make rudimentary attributes of user model.

--- app/models/user.js
+++ app/models/user.js
@@ -1,5 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+  username: DS.attr(),
+  password: DS.attr(),
+  email: DS.attr(),
+  habits: DS.hasMany('habit', {async: true})
 });
