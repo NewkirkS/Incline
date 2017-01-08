5cb471f4e8619b792212a7d0f1b78582016e1a93
Stephen Newkirk <newkirk771@gmail.com>
2017-01-08 10:15:29 -0800

    Flesh out basic create user action in index route.

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,20 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  // model() {
-  //   return Ember.RSVP.hash({
-  //     //If session user === true, make user = user
-  //
-  //     //If user = true, return habits where habits.user = user
-  //
-  //   });
-  // },
-  // actions: {
-  //   createUser() {
-  //
-  //   },
-  //   loginUser() {
-  //
-  //   },
-  // }
+  model() {
+    return Ember.RSVP.hash({
+      //If user = true, return habits where habits.user = user
+
+    });
+  },
+  actions: {
+    createUser(params) {
+      //validate username
+      //validate password
+      //hash password and set params.password = to hash
+      var newUser = this.store.createRecord('user', params);
+      newUser.save();
+      this.loginUser(params);
+    },
+    loginUser(params) {
+      
+    },
+  }
 });
--- app/services/authentication.js
+++ app/services/authentication.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Service.extend({
+  authenticatied: false,
+  user: null,
+  logIn(params) {
+    //retrieve user record where username = input
+    //if password hash in db = hash of password input make user = db user
+    //else error
+  },
+  logOut() {
+    this.toggleProperty("authenticated");
+    this.set("user", null);
+  }
 });
