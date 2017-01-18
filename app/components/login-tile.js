740b58ec8778a9b7f520a791859857f2d97a0ac2
Stephen Newkirk <newkirk771@gmail.com>
2017-01-18 08:04:38 -0800

    Clean up login-tile.js file.

--- app/components/login-tile.js
+++ app/components/login-tile.js
@@ -3,19 +3,6 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   session: Ember.inject.service(),
   actions: {
-    createUser() {
-      var params = {
-        username: this.get('new-username'),
-        password: this.get('new-password')
-      };
-      if (params.username === undefined || params.password === undefined) {
-        alert("Please fill out all fields.");
-      } else {
-        this.sendAction('createUser', params);
-        this.set('username', undefined);
-        this.set('password', undefined);
-      }
-    },
     signIn() {
       this.sendAction('signIn', "google");
     },
