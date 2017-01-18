dc2afcc68785d3473d58ff225a56312cfde24a51
Stephen Newkirk <newkirk771@gmail.com>
2017-01-18 08:30:50 -0800

    Complete auth migration to navbar.

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,8 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  session: Ember.inject.service(),
+  beforeModel() {
+    return this.get('session').fetch().catch(function() {});
+  },
   actions: {
+    signIn() {
+      this.get('session').open('firebase', {provider: "google"}).then(function(data) {
+        console.log(data);
+      });
+    },
     signOut() {
       this.get('session').close();
       this.transitionTo('index');
