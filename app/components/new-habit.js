a265f838e5b1fceaf4b949a3c8e63962bb0bbdeb
Stephen Newkirk <newkirk771@gmail.com>
2017-01-19 12:05:35 -0800

    Make component level function for creating new habits.

--- app/components/new-habit.js
+++ app/components/new-habit.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  session: Ember.inject.service(),
+
+  actions: {
+    newHabit() {
+      var params = {
+        name: this.get("name"),
+      };
+      if (params.name === undefined) {
+        alert("Please enter a name for your new habit.");
+      } else {
+        this.sendAction("newHabit", params);
+      }
+    }
+  }
 });
