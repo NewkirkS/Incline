71dc4cc6ebfaa891c7c65cd76286ade92f46c53c
Stephen Newkirk <newkirk771@gmail.com>
2017-01-29 12:34:57 -0800

    Configure dummy chart to use calendar chart type.

--- app/routes/habit.js
+++ app/routes/habit.js
@@ -67,33 +67,58 @@ export default Ember.Route.extend({
     renderChart() {
 
 
-    var data = {
-    "type":"line",
-    "title":{
-      "text":"Average Metric"
-    },
-    "series":[
-      {
-        "values":[69,68,54,48,70,74,98,70,72,68,49,69]
-      },
-      {
-        "values":[51,53,47,60,48,52,75,52,55,47,60,48]
-      },
-      {
-        "values":[42,43,30,40,31,48,55,46,48,32,38,38]
-      },
-      {
-        "values":[25,15,26,21,24,26,33,25,15,25,22,24]
+    var myConfig = {
+      "type":"calendar",
+      "options":{
+        "year":{
+          "text":2016,
+          "font-color":"#666699",
+          "font-family":"Georgia, serif",
+        },
+      "start-month":1,
+      "end-month":12,
+      "firstday":"s",
+      "values":[  //Provide your values data.
+        ["2016-04-01",1, "April Fool's Day!"],
+        ["2016-04-03",-2],
+        ["2016-04-07",-1],
+        ["2016-04-09",2],
+        ["2016-04-13",5],
+        ["2016-04-15",-4],
+        ["2016-04-16",-5],
+        ["2016-04-17",1],
+        ["2016-04-28",3],
+        ["2016-04-30",-3],
+        ["2016-05-01",2, "May Flowers!"],
+        ["2016-05-04",-1],
+        ["2016-05-06",2],
+        ["2016-05-07",3],
+        ["2016-05-08",-5],
+        ["2016-05-11",4],
+        ["2016-05-12",-3],
+        ["2016-05-20",-4],
+        ["2016-05-22",5],
+        ["2016-05-26",-3],
+        ["2016-05-28",2],
+        ["2016-06-01",3],
+        ["2016-06-03",-2],
+        ["2016-06-04",-4],
+        ["2016-06-09",5]
+        ],
+        "plotarea":{
+	  "margin-top":"30%",
+	  "margin-bottom":"50%"
+	}
       }
-    ]
-  };
+    };
 
-      return zingchart.render({
-        id: "chartDiv",
-        data: data,
-        height: 400,
-        width: 600
-      });
+    return zingchart.render({
+      id : 'myChart',
+      data : myConfig,
+      height: 150,
+      width: 700,
+      modules : 'calendar'
+    });
     },
   }
 });
